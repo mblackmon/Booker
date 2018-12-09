031286eccc94c8b90f6ea0e63a9c15523e182beb
Matt Blackmon <m.h.blackmon@gmail.com>
2018-12-09 13:24:09 -0600

    Add model and parsing tests

---
+++ Booker/Models.swift
@@ -0,0 +1,42 @@
+//
+//  Models.swift
+//  Booker
+//
+//  Created by mblackmon on 12/9/18.
+//  Copyright © 2018 None. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class Book: Object {
+    @objc dynamic var givenKey = ""
+    @objc dynamic var name = ""
+    @objc dynamic var author = ""
+    @objc dynamic var isSaved = false
+    
+    override static func primaryKey() -> String {
+        return "givenKey"
+    }
+}
+
+//MARK: Extension to interact with parsing
+extension Book {
+    convenience init(from parsed: ParsedBook) {
+        self.init()
+        givenKey = parsed.key
+        name = parsed.titleSuggest
+        
+        isSaved = false
+    }
+}
+
+struct ParsedBook: Decodable {
+    let titleSuggest: String
+    let key: String
+    let authorName: [String]
+}
+
+struct SearchJSONResponse: Decodable {
+    let docs: [ParsedBook]
+}
--- BookerTests/BookerTests.swift
+++ BookerTests/BookerTests.swift
@@ -19,15 +19,39 @@ class BookerTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testParsingSearchResponse() {
+        guard let json = testJsonFor("testSearch") else {
+            XCTFail("cannot proceed without testSearch.json file")
+            return
+        }
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        guard let response = try? decoder.decode(SearchJSONResponse.self, from: json) else {
+            XCTFail("could not decode json search response")
+            return
+        }
+        debugPrint(#file, response.docs.count)
+        //note: I usually use Nimble, so please forgive if this is not idiomatic
+        XCTAssertTrue(response.docs.count == 93, "Expected 93 results")
+        XCTAssertTrue(response.docs.first?.titleSuggest == "The Hobbit", "Expected The Hobbit")
+        XCTAssertTrue(response.docs.first?.authorName.contains("J. R. R. Tolkien") ?? false , "Wrong Author Name")
+        XCTAssertTrue(response.docs.first?.key == "/works/OL262758W", "Wrong key")
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    //MARK: Test Helper Functions
+    func testJsonFor(_ name: String) -> Data? {
+        let bundle = Bundle(for: type(of: self))
+        let jsonPath = bundle.path(forResource: name, ofType: ".json")
+        guard let path = jsonPath else {
+            XCTFail("couldn't load resource for this test, can't find \(name).json in bundle")
+            return nil
+        }
+        do {
+            let data = try Data(contentsOf: URL(fileURLWithPath: path))
+            return data
+        } catch {
+            XCTFail("couldn't load data from raw json file at \(path)")
+            return nil
         }
     }
 
